151f744035b1bacc4898a9485673cd1dcb242f61
flobreiti <84981609+flobreiti@users.noreply.github.com>
2021-06-02 08:48:30 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Mein Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+      name: `Florian`,
+      summary: `ich liebe mein leben`,
   },
   plugins: [
     `gatsby-plugin-image`,
